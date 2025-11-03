ae4e34013b5e5068f8739b7d74a447ff8eb685fe
Bishal Dahal <bishaldahal4777@gmail.com>
2025-11-03 19:12:29 +0545

    new commit

---
+++ app/src/main/java/com/example/mobileprogramming/LoginActivity.java
@@ -0,0 +1,61 @@
+package com.example.mobileprogramming;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText edtEmail, editPassword;
+
+    private CheckBox chkRememberMe;
+
+    private AppCompatButton btnLogin;
+
+    private TextView txtForgotPassword;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    private void findViews(){
+        edtEmail = findViewById(R.id.edtEmail);
+        editPassword =findViewById(R.id.editPassword);
+        chkRememberMe = findViewById(R.id.chkRememberMe);
+        btnLogin = findViewById(R.id.btnLogin);
+        txtForgotPassword = findViewById(R.id.txtForgotPassword);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edtEmail.getText().toString().trim();
+                String password = editPassword.getText().toString().trim();
+
+                if(email.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Email cannot be empty", Toast.LENGTH_SHORT).show();
+                }else if(password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Password cannot be empty", Toast.LENGTH_SHORT).show();
+
+                }else if (email.equals("bishal@gmail.com") && password.equals("bishal123")){
+                    Toast.makeText(LoginActivity.this, "login Successful", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(LoginActivity.this, "login Credential cannot match", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
