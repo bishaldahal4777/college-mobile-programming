846bd5721423e42558dd113de4d4e1f2b3ebdee5
Bishal Dahal <bishaldahal4777@gmail.com>
2025-11-04 07:23:42 +0545

    add signup view in login activity

--- app/src/main/java/com/example/mobileprogramming/LoginActivity.java
+++ app/src/main/java/com/example/mobileprogramming/LoginActivity.java
@@ -14,7 +14,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText edtEmail, editPassword;
 
@@ -32,9 +32,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
-    private void findViews(){
+    private void findViews() {
         edtEmail = findViewById(R.id.edtEmail);
-        editPassword =findViewById(R.id.editPassword);
+        editPassword = findViewById(R.id.editPassword);
         chkRememberMe = findViewById(R.id.chkRememberMe);
         btnLogin = findViewById(R.id.btnLogin);
         txtForgotPassword = findViewById(R.id.txtForgotPassword);
@@ -42,30 +42,29 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         txtsignup.setOnClickListener(this);
         btnLogin.setOnClickListener(this);
 
-        });
 
     }
 
     @Override
     public void onClick(View view) {
-        if(view.getId()==R.id.btnLogin){
-            btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = edtEmail.getText().toString().trim();
-                String password = editPassword.getText().toString().trim();
+        if (view.getId() == R.id.btnLogin) {
 
-                if(email.isEmpty()){
-                    Toast.makeText(LoginActivity.this, "Email cannot be empty", Toast.LENGTH_SHORT).show();
-                }else if(password.isEmpty()) {
-                    Toast.makeText(LoginActivity.this, "Password cannot be empty", Toast.LENGTH_SHORT).show();
+            String email = edtEmail.getText().toString().trim();
+            String password = editPassword.getText().toString().trim();
 
-                }else if (email.equals("bishal@gmail.com") && password.equals("bishal123")){
-                    Toast.makeText(LoginActivity.this, "login Successful", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(LoginActivity.this, "login Credential cannot match", Toast.LENGTH_SHORT).show();
-                }
+            if (email.isEmpty()) {
+                Toast.makeText(LoginActivity.this, "Email cannot be empty", Toast.LENGTH_SHORT).show();
+            } else if (password.isEmpty()) {
+                Toast.makeText(LoginActivity.this, "Password cannot be empty", Toast.LENGTH_SHORT).show();
+
+            } else if (email.equals("bishal@gmail.com") && password.equals("bishal123")) {
+                Toast.makeText(LoginActivity.this, "login Successful", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(LoginActivity.this, "login Credential cannot match", Toast.LENGTH_SHORT).show();
             }
+
+        } else if (view.getId() == R.id.txtSignup) {
+
         }
     }
 }
