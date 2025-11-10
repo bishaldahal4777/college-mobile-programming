836d53f98006aa499e1e3d5554cbef44f3c1cb37
Bishal Dahal <bishaldahal4777@gmail.com>
2025-11-10 06:41:41 +0545

    new commit

--- app/src/main/java/com/example/mobileprogramming/LoginActivity.java
+++ app/src/main/java/com/example/mobileprogramming/LoginActivity.java
@@ -56,6 +56,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
             if (email.isEmpty()) {
                 Toast.makeText(LoginActivity.this, "Email cannot be empty", Toast.LENGTH_SHORT).show();
+            } else if (password.isEmpty()) {
+                Toast.makeText(LoginActivity.this, "Password cannot be empty", Toast.LENGTH_SHORT).show();
+
             } else if (email.equals("bishal@gmail.com") && password.equals("bishal123")) {
                 Toast.makeText(LoginActivity.this, "login Successful", Toast.LENGTH_SHORT).show();
             } else {
