512848752dd2933a6a2668dd9f7b63bc0f9408e3
Bishal Dahal <bishaldahal4777@gmail.com>
2025-11-10 07:20:15 +0545

    signup activity java

--- app/src/main/java/com/example/mobileprogramming/ActivitySignup.java
+++ app/src/main/java/com/example/mobileprogramming/ActivitySignup.java
@@ -1,15 +1,25 @@
 package com.example.mobileprogramming;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ActivitySignup extends AppCompatActivity {
+    private EditText edtFullName, edtEmail, edtPassword, edtConfirmPassword;
+    private Spinner spinnerGender;
+    private AppCompatButton btnSignup;
+    private TextView txtLogin;
+
+    private String selectedGender = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
